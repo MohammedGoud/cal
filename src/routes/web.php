b0e0d6050290bbfcf91b14a155be1a94c3c1a102
Mohammed AboElgoud <mohammed.aboelgoud@westore.ai>
2022-05-29 20:46:07 +0200

    Fix

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,5 +1,18 @@
 <?php
 
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+use Gouda\Calculator\CalculatorController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,11 +26,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('add/{a}/{b}', [CalculatorController::class,'add']);
